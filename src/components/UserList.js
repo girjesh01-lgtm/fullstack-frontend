d5b32bbe66b5d192895dfa6a982df6bc76c24b9c
Girjesh Kumar <girjesh01@gmail.com>
2025-12-05 17:21:47 +0530

    Day 2: work

---
+++ src/components/UserList.js
@@ -0,0 +1,23 @@
+import { useEffect, userState, useState } from "react";
+
+function UserList() {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/api/hello")
+        .then(r => r.text())
+        .then(data => setMessage(data));
+        //.then(data => console.log(data))
+        //.catch(err => console.error(err));
+    }, []);
+
+
+    return (
+        <div>
+            <h2>Backend Says:</h2>
+            <p>{message}</p>
+        </div>
+    );
+}
+
+export default UserList;
